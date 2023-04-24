9e6b720baeff7b463e650ab10882629347151402
rakeshbsw2022 <rakeshbsw2022@gmail.com>
2023-04-24 22:46:38 +0530

    commit for data ingestion

--- src/exception.py
+++ src/exception.py
@@ -19,8 +19,8 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    
-    
-
+ 
+"""
 if __name__=="__main__":
     logging.info("Logging has started")
     try:
@@ -28,4 +28,5 @@ if __name__=="__main__":
     except Exception as e:
         logging.info('Dicision by zero') 
         raise CustomException(e,sys)
+"""
 
